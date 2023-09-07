33880ba0e2bcad007aaeaa40ae062ec33c7d2d91
eobcre <eobcre@gmail.com>
2023-09-07 11:37:58 -0400

    created layout

---
+++ src/components/Time.tsx
@@ -0,0 +1,9 @@
+const Time = () => {
+  return (
+    <div className='flex justify-center items-center mt-10 mb-4'>
+      <p className='text-white text-[36px]'>00:00:00</p>
+    </div>
+  );
+};
+
+export default Time;
