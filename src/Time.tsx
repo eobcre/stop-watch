db2d813c78f05d85f1f784f967b24ef260ce96d1
eobcre <eobcre@gmail.com>
2023-09-07 11:36:40 -0400

    created new component

--- src/Time.tsx
+++
@@ -1,9 +0,0 @@
-const Time = () => {
-  return (
-    <div className='flex justify-center items-center mt-10 mb-4'>
-      <p className='text-white text-[36px] font-Rubik'>00:00:00</p>
-    </div>
-  );
-};
-
-export default Time;
