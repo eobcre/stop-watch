4d247d90cf6e14ae36099b1335cc9683eaaf6fd3
eobcre <eobcre@gmail.com>
2023-09-07 10:09:27 -0400

    updated style, placed component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import Button from './components/Button';
+
 const App = () => {
   return (
     <div className='flex justify-center items-center bg-[#5C6877] h-screen'>
-      <div className='bg-[#303030] rounded w-[400px] h-[480px]'></div>
+      <div className='bg-[#303030] rounded w-[400px] h-[480px]'>
+        <Button />
+      </div>
     </div>
   );
 };
