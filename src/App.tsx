9ffc0c2b91b38c95845990cafe6ca28a9e55dc74
eobcre <eobcre@gmail.com>
2023-09-06 23:02:33 -0400

    background styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className='flex justify-center items-center bg-[#5C6877] h-screen'>
+      <div className='bg-[#303030] rounded w-[400px] h-[480px]'></div>
+    </div>
+  );
 };
 
 export default App;
