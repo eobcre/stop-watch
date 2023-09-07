ea8d471a396d080566fd4a56c18b0b872066e8fe
eobcre <eobcre@gmail.com>
2023-09-07 11:37:26 -0400

    created layout

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,5 +1,19 @@
 const List = () => {
-  return <div>List</div>;
+  return (
+    <div className='py-12'>
+      <ul>
+        <li className='flex justify-between text-white pl-7 pr-36'>
+          <span className='py-3'>Lap</span>
+          <span className='py-3'>Time</span>
+        </li>
+
+        <li className='flex justify-between text-white pl-7 pr-28'>
+          <span>1</span>
+          <span>00:00:00</span>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default List;
