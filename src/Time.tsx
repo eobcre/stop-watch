d9078acbcb65d913c3de2d0dd870ece1440970bb
eobcre <eobcre@gmail.com>
2023-09-07 10:35:00 -0400

    styled

--- src/Time.tsx
+++ src/Time.tsx
@@ -1,5 +1,9 @@
 const Time = () => {
-  return <div>Time</div>;
+  return (
+    <div className='flex justify-center items-center mt-10 mb-4'>
+      <p className='text-white text-[36px] font-Rubik'>00:00:00</p>
+    </div>
+  );
 };
 
 export default Time;
