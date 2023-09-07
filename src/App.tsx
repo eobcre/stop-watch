60b6b58e2955031f4b94fbe0c07649d33f27b90c
eobcre <eobcre@gmail.com>
2023-09-07 10:21:14 -0400

    placed components, styled and props

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,11 @@ const App = () => {
   return (
     <div className='flex justify-center items-center bg-[#5C6877] h-screen'>
       <div className='bg-[#303030] rounded w-[400px] h-[480px]'>
-        <Button />
+        <div className='flex justify-center gap-5'>
+          <Button name='Start' className='bg-[#35713B]' />
+          <Button name='Lap' className='bg-[#4C7EC1]' />
+          <Button name='End' className='bg-[#AB3839]' />
+        </div>
       </div>
     </div>
   );
