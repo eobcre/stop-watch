53d2c9abb989391d8ed7edc86a0496a7894404f2
eobcre <eobcre@gmail.com>
2023-09-12 12:07:31 -0400

    type for props, props passed

--- src/components/Time.tsx
+++ src/components/Time.tsx
@@ -1,7 +1,11 @@
-const Time = () => {
+type TimeProps = {
+  formatTime: () => string;
+};
+
+const Time: React.FC<TimeProps> = ({ formatTime }) => {
   return (
     <div className='flex justify-center items-center mt-10 mb-4'>
-      <p className='text-white text-[36px]'>00:00:00</p>
+      <p className='text-white text-[36px]'>{formatTime()}</p>
     </div>
   );
 };
