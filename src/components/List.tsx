705c36f45e57f044eae0ccc175561d162be33a21
eobcre <eobcre@gmail.com>
2023-09-12 12:06:55 -0400

    time format display logic, props passed

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -3,6 +3,18 @@ type ListProps = {
 };
 
 const List: React.FC<ListProps> = ({ lapTimes }) => {
+  const formatListTime = (timeInSeconds: number) => {
+    const hours = Math.floor(timeInSeconds / 3600);
+    const minutes = Math.floor((timeInSeconds % 3600) / 60);
+    const seconds = timeInSeconds % 60;
+
+    const formatHours = hours.toString().padStart(2, '0');
+    const formatMinutes = minutes.toString().padStart(2, '0');
+    const formatSeconds = seconds.toString().padStart(2, '0');
+
+    return `${formatHours}:${formatMinutes}:${formatSeconds}`;
+  };
+
   return (
     <div className='py-12'>
       <ul>
@@ -13,17 +25,19 @@ const List: React.FC<ListProps> = ({ lapTimes }) => {
       </ul>
 
       <ul>
-        {lapTimes.map((lapTime, index) => (
-          <div
-            key={index}
-            className={`flex justify-between text-white pl-7 pr-28 ${
-              index % 2 == 0 ? 'bg-[#4D7DC1]' : ''
-            }`}
-          >
-            <li>{index + 1}</li>
-            <li>{lapTime} seconds</li>
-          </div>
-        ))}
+        {lapTimes.map((lapTime, index) => {
+          return (
+            <div
+              key={index}
+              className={`flex justify-between text-white pl-7 pr-28 py-1 ${
+                index % 2 === 0 ? 'bg-[#4D7DC1] p-1' : ''
+              }`}
+            >
+              <li>{index + 1}</li>
+              <li>{formatListTime(lapTime)}</li>
+            </div>
+          );
+        })}
       </ul>
     </div>
   );
