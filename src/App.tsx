6fa1a8ce7e83acd54ba13f02ada07794d4885adb
eobcre <eobcre@gmail.com>
2023-09-12 12:05:03 -0400

    state variable, function name changed for readability, created timer logic, lap, end button logic, time format display logic and props passed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import Time from './components/Time';
 import Button from './components/Button';
@@ -7,27 +7,65 @@ import List from './components/List';
 const App: React.FC = () => {
   const [time, setTime] = useState<number>(0);
   const [running, setRunning] = useState<boolean>(false);
-  const [lap, setLap] = useState([]);
+  const [lapTimes, setLapTimes] = useState<number[]>([]);
 
-  // start, stop
-  const startStop = () => {
+  useEffect(() => {
+    let interval: number | undefined;
+
+    if (running) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [running]);
+
+  // start, stop button
+  const handleStartStop = () => {
     setRunning(!running);
+  };
+
+  // lap button
+  const handleLap = () => {
+    setLapTimes([...lapTimes, time]);
+  };
+
+  // end, reset button
+  const handleReset = () => {
+    setTime(0);
+    setLapTimes([]);
+  };
+
+  // format time display
+  const formatTime = (timeInSeconds: number) => {
+    const hours = Math.floor(timeInSeconds / 3600);
+    const minutes = Math.floor((timeInSeconds % 3600) / 60);
+    const seconds = timeInSeconds % 60;
+
+    return `${padTime(hours)}:${padTime(minutes)}:${padTime(seconds)}`;
+  };
+
+  const padTime = (value: number) => {
+    return value < 10 ? `0${value}` : `${value}`;
   };
 
   return (
     <div className='flex justify-center items-center bg-[#5C6877] h-screen'>
       <div className='bg-[#303030] rounded w-[400px] h-[480px]'>
-        <Time />
+        <Time formatTime={() => formatTime(time)} />
         <div className='flex justify-center gap-5'>
           <Button
-            onClick={startStop}
+            onClick={handleStartStop}
             name={running ? 'Stop' : 'Start'}
             className='bg-[#35713B]'
           />
-          <Button name='Lap' className='bg-[#4C7EC1]' />
-          <Button name='End' className='bg-[#AB3839]' />
+          <Button onClick={handleLap} name='Lap' className='bg-[#4C7EC1]' />
+          <Button onClick={handleReset} name='End' className='bg-[#AB3839]' />
         </div>
-        <List />
+        <List lapTimes={lapTimes} />
       </div>
     </div>
   );
